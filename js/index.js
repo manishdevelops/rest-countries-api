c672fec29363f676a4f03d793ccf92bf7bdf652d
manish mandal <76776194mm@gmail.com>
2022-11-27 22:40:26 +0530

    update all

--- js/index.js
+++ js/index.js
@@ -3,16 +3,23 @@
 const dropdownBtn = document.querySelector('.dropdown-btn');
 const dropdownItems = document.querySelector('.dropdown-items');
 const arrowIcon = document.querySelector('.arrow');
+const themeBtn = document.querySelector('.main__header--themeToggle');
+const Body = document.querySelector('body');
+console.log(Body);
 
 class App {
   constructor() {
     dropdownBtn.addEventListener('click', this._dropdownToggle)
+    themeBtn.addEventListener('click', this._themeChange);
   }
 
   _dropdownToggle() {
     dropdownItems.classList.toggle('dropdown-hide');
-    // arrowIcon.toggleAttribute("animation");
     arrowIcon.classList.toggle('arrowNotAnimate');
+  }
+
+  _themeChange() {
+    Body.classList.toggle('dark-theme');
   }
 
 }
